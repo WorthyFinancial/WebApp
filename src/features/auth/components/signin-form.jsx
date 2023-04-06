3ca7dafcb926023794d24492915bf666c901d26f
Thelma Boamah <thelma.a.boamah@gmail.com>
2023-04-06 05:49:22 -0400

    replaces class with className in a few components

--- src/features/auth/components/signin-form.jsx
+++ src/features/auth/components/signin-form.jsx
@@ -1,9 +1,9 @@
 export const SigninForm = () => {
     return (
-        <article class="br3 shadow-5 ba dark-gray b--black-10 mv4 w-100 w-50-m w-25-l mw5 center">
+        <article className="br3 shadow-5 ba dark-gray b--black-10 mv4 w-100 w-50-m w-25-l mw5 center">
             <main className="pa4 black-80">
                 <form className="measure ">
-                    <fieldset id="sign_up" class="ba b--transparent ph0 mh0">
+                    <fieldset id="sign_up" className="ba b--transparent ph0 mh0">
                     <legend className="f2 fw6 ph0 mh0">Sign In</legend>
                     <div className="mt3">
                         <label className="db fw6 lh-copy f6" for="email-address">Email</label>
@@ -19,8 +19,8 @@ export const SigninForm = () => {
                     <input className="b ph3 pv2 input-reset ba b--black bg-transparent grow pointer f6 dib" type="submit" value="Sign in" />
                     </div>
                     <div className="lh-copy mt3">
-                    <a href="#0" class="f6 link dim black db">Sign up</a>
-                    <a href="#0" class="f6 link dim black db">Forgot your password?</a>
+                    <a href="#0" className="f6 link dim black db">Sign up</a>
+                    <a href="#0" className="f6 link dim black db">Forgot your password?</a>
                     </div>
                 </form>
                 </main>
